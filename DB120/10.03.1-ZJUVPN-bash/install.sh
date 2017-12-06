890e4a50a8f3f48763589f0e5385acb3898552a7
shmilee <shmilee.zju@gmail.com>
2017-12-06 15:06:38 +0800

    rm DB120/{10.03.1,14.07}, WNDR3700v4-14.07

--- DB120/10.03.1-ZJUVPN-bash/install.sh
+++
@@ -1,6 +0,0 @@
-#http://openwrt.proxy.ustclug.org/backfire/10.03.1/brcm63xx/packages/
-opkg install libncurses_5.7-2_brcm63xx.ipk bash_4.2-1_brcm63xx.ipk
-opkg install xl2tpd_1.3.0-1_brcm63xx.ipk
-tar xvf vpn-arch.tar -C /
-cat ./firewall.user >>/etc/firewall.user
-echo vi /usr/sbin/vpn-connect about xl2tpd
